fe80b1408276b8de1ffdba20d9f53c40ace24815
MarenWoodruff <wren403@gmail.com>
2017-01-08 20:32:16 -0500

    finished app

---
+++ routes/index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
+var router = express.Router();
+
+
+router.get('/', function (req, res) {
+    res.render('index', { user : req.user });
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
+
+router.post('/register', function(req, res) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { account : account });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            req.session.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect('/');
+            });
+        });
+    });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login', { user : req.user, error : req.flash('error')});
+});
+
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), (req, res, next) => {
+    req.session.save((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    req.session.save((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
+router.get('/ping', function(req, res){
+    res.status(200).send("pong!");
+});
+
+module.exports = router;
